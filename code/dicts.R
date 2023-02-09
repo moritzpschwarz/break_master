1045649af2ea7ee63da186b339970714f3de68a5
ebbam <99182071+ebbam@users.noreply.github.com>
2023-02-09 10:24:10 +0000

    Dictionaries and data files

--- code/dicts.R
+++ code/dicts.R
@@ -1,3 +1,7 @@
+# Country groupings
+
+cty_sample <- readRDS(here("data/out/country_sample.RDS"))
+
 EU15 <- c("Austria", "Belgium", "Germany", "Denmark", "Spain", "Finland",
           "France", "United Kingdom", "Ireland", "Italy", "Luxembourg", 
           "Netherlands", "Greece", "Portugal", "Sweden")
@@ -9,8 +13,34 @@ EU31 <- c("Austria", "Croatia", "Belgium", "Bulgaria", "Cyprus",
           "Portugal", "Romania", "Slovak Republic", "Slovenia", "Sweden", 
           "Switzerland", "Iceland", "Norway")
 
-OECD <- readRDS(here("data/out/oecd_countries.rds"))
 
+# Developing vs Developed: 4 x AC6 & AC1
+# Source: https://www.oecd-ilibrary.org/sites/f0773d55-en/1/4/3/index.html?itemId=/content/publication/f0773d55-en&_csp_=5026909c969925715cde6ea16f4854ee&itemIGO=oecd&itemContentType=book
+AC6 <- read_excel(here("data/raw/country_groupings.xlsx"), sheet = 2)
+AC1 <- read_excel(here("data/raw/country_groupings.xlsx")) %>% pull(Developed) %>% unlist
+for(i in 1:nrow(AC6)){
+  temp <- AC6 %>% slice(i) %>% pull(countries) %>% strsplit(., ", ") %>% unlist
+  mv(from = "temp", to = paste0("AC6_", AC6$cat_abbrev[i]))
+}
+AC6_HICs <- c(AC6_HICs, "Russia")
+
+# AC1 -> Eastern Europe vs. Rest
+east_euro <- setdiff(EU31[!(EU31 %in% c("Switzerland", "Iceland", "Norway", "Cyprus", "Malta"))], EU15)
+euro_main <- setdiff(EU31[EU31 != "Cyprus"], east_euro)
+
+# OECD
+OECD <- readRDS(here("data/out/oecd_countries.rds")) %>% 
+  replace(. == "Korea", "South Korea")
+
+# Emerging
+emerging <- c("Argentina", "Brazil", "China", "Chile", "Costa Rica", "Colombia", 
+              "India", "Indonesia", "Peru", "Russia", "Mexico", "South Africa", 
+              "Turkey")
+
+# Combine
+samples <- mget(c("EU15", "EU31", "AC1", "AC6_HICs", "AC6_UMICs", "AC6_LMICs", "east_euro", "euro_main", "OECD", "emerging"))
+
+#### Name standardisation
 country_dict <- readRDS(here("data/out/countrydict.RDS"))
 #saveRDS(country_dict, "data/out/countrydict.RDS")
 
@@ -26,78 +56,27 @@ get_country <- function(c_name){
   }else{return(c_name)}
   }
 
-test_diff <- function(df1 = data.frame, df2 = data.frame, vars = list){
-  for(i in 1:length(vars)){
-    a <- df1 %>% pull(vars[i])
-    b <- df2 %>% pull(vars[i])
-    if(length(setdiff(a,b) != 0)) {
-      print(paste(deparse(substitute(df1)),"has extra:",setdiff(a,b)))
-    }else{print(paste(vars[i],"match"))}
-    if(length(setdiff(b,a) != 0)) {
-      print(paste(deparse(substitute(df2)),"has extra:",setdiff(b,a)))
-    }else{print(paste(vars[i],"match"))}
-  }
-}
 
-# test_complete <- function(df){
-#   is.pbalanced(df) %>% print
-#   as.logical(n_distinct(df$country) == 51)
-#   df %>% count(country) %>% filter(n != 52) %>% nrow(.) == 0
-#   df %>% count(year) %>% filter(n != 51) %>% nrow(.) == 0
-#   cty_sample[!(cty_sample %in% df$country)] %>% length == 0
-# }
-
-# Heat map generation
-gen_country <- function(df){
-  country_p <- df %>% 
-    pivot_longer(cols = !c(country, year), values_drop_na = TRUE) %>% 
-    filter(value > 0)  %>% 
-    group_by(name, year) %>% 
-    summarise(obs = n(), n_OECD = sum(country %in% union(EU31, OECD))) %>% 
-    ggplot(., aes(x = year)) + 
-    geom_col(aes(y = obs, fill = "blue")) +
-    geom_col(aes(y = n_OECD, fill = "green")) +
-    geom_hline(yintercept = 43, linetype = "dashed") +
-    facet_wrap(~name) +
-    theme(legend.position = "none")
-  return(country_p)
-}
-
-gen_year <- function(df){
-  year_p <- df %>% 
-    pivot_longer(cols = !c(country, year), values_drop_na = TRUE) %>% 
-    filter(value > 0)  %>% 
-    group_by(name) %>% 
-    summarise(start = as.numeric(min(year)), end = as.numeric(max(year))) %>% 
-    ggplot() +
-    aes(y = name) +
-    geom_segment(aes(x = start, 
-                     xend = end,
-                     yend = name,
-                     color = name), 
-                 size = 5,
-                 show.legend = FALSE) +
-    geom_text(aes(x = start, label = start), 
-              nudge_x = 0,
-              size = 3) +
-    geom_text(aes(x = end, label = end), 
-              nudge_x = 0,
-              size = 3)
-  return(year_p)
-}
-
-
-# Functions for results overview
-get_model <- function(ct_sample, yr_range, p.val, l_iis, b_sz){
-  m <- models %>% filter(country_sample == ct_sample & 
-                           year_range == yr_range & 
-                           p_val == p.val & 
-                           iis == l_iis & 
-                           b_size == b_sz)
-  mod <- m %>% pull(is) %>% first()
-  p <- mod %>% plot(zero_line = FALSE)
-  p_grid <- mod %>% plot_grid
-  mt <- m$main_title
-  st <- m$sub_title
-  return(list(results = mod, plot = p, plot_grid = p_grid, main_title = mt, sub_title = st))
+# Checks completeness of data frames in cleaning/combining master_cleaning.Rmd
+test_complete <- function(df){
+  stopifnot(
+    # Balanced
+    is.pbalanced(df),
+    # 51 distinct countries
+    n_distinct(df$country) == 51,
+    # All cty_sample countries represented
+    length(cty_sample[!(cty_sample %in% df$country)]) == 0,
+    # At least 20 observations per country
+    df %>% count(country) %>% filter(n < 20) %>% nrow(.) == 0,
+    # 51 country observations per year
+    df %>% count(year) %>% filter(n != 51) %>% nrow(.) == 0)
+  print("Passed.")
+  
+  # Complete cases check
+  tmp <- df %>% filter(year >= 2000 & !complete.cases(.))
+  if(nrow(tmp) == 0){"No missing cases."}else{
+    print("Missing cases:")
+          print(tmp)
+          }
+  
 }
